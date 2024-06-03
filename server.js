3a7398847a1f0b60fa3e4cdaf522a6ea6c0e8c6f
rellagant <barbara.cs@gmail.com>
2024-06-03 15:44:34 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+app.use("/", shrinesRouter);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
