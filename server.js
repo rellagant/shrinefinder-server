8e963267212ec72a1a0d33ae3f20b5a91d567807
rellagant <barbara.cs@gmail.com>
2024-06-03 20:40:51 -0400

    added preliminary data file; added static file serving from public directory

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static("public"));
+
 app.use("/", shrinesRouter);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
