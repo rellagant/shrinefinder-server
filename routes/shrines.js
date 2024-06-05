8c32131220c6a9449b538046a557fb408b2b27fb
rellagant <barbara.cs@gmail.com>
2024-06-05 14:51:52 -0400

    added case insensitive validation for city query param; added function to filter shrines by property; added GET endpoint for shrines by city

--- routes/shrines.js
+++ routes/shrines.js
@@ -16,7 +16,7 @@ router.get("/", (_req, res) => {
   res.json(shrines);
 });
 
-// GET endpoint for individual shrines by id
+// GET endpoint for individual shrine by id
 router.get("/:id", (req, res) => {
   const shrine = readShrines();
   const singleShrine = shrine.find((shrine) => shrine.id === req.params.id);
@@ -26,6 +26,40 @@ router.get("/:id", (req, res) => {
   } else {
     res.json(singleShrine);
   }
+});
+
+// GET endpoint for shrines by city
+
+// validation for city parameter case insensitive
+const checkCityParam = (req, res, next) => {
+  const city = req.params.city;
+
+  if (!city || typeof city !== "string" || city.trim().length === 0) {
+    return res.status(400).json({ error: "Invalid city parameter" });
+  }
+  next();
+};
+
+// Function to filter shrines by property
+const findShrinesByProperty = (property, value) => {
+  const shrines = readShrines();
+  return shrines.filter(
+    (shrine) => shrine[property].toLowerCase() === value.toLowerCase()
+  );
+};
+
+// Get endpoint by city
+router.get("/byCity/:city", checkCityParam, (req, res) => {
+  const city = req.params.city;
+  const shrinesInCity = findShrinesByProperty("city", city);
+
+  if (shrinesInCity.length === 0) {
+    return res.status(404).json({
+      error:
+        "No shrines found at the moment, please come back later as more shrines will be added!",
+    });
+  }
+  res.json(shrinesInCity);
 });
 
 module.exports = router;
---
+++ utils/shrineUtils.js
@@ -0,0 +1,9 @@
+// const shrines = require("../data/shrines.json");
+
+// const findShrinesbyProperty = (property, value) => {
+//     return shrines.filter(shrine =shrine[property].toLowerCase() === value.toLowerCase());
+// }
+
+// module.exports = {
+//     findShrinesbyProperty
+// }
