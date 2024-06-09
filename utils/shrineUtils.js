69e6f0889294e1a0e76d156e16aab6ff74762896
rellagant <barbara.cs@gmail.com>
2024-06-09 19:45:13 -0400

    removed utils

--- utils/shrineUtils.js
+++
@@ -1,9 +0,0 @@
-// const shrines = require("../data/shrines.json");
-
-// const findShrinesbyProperty = (property, value) => {
-//     return shrines.filter(shrine =shrine[property].toLowerCase() === value.toLowerCase());
-// }
-
-// module.exports = {
-//     findShrinesbyProperty
-// }
